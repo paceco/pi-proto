db258f1b973f612c15dd7bc7f9ad8f68c914a8e6
Nathan Long <nlong5@gmail.com>
2015-11-03 10:26:43 -0500

    updating pi-proto to latest patterns

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,12 @@ var livereload  = require('gulp-livereload');
 var express     = require('express');
 
 
+// Asset Handling - SCSS and JS
+// --------------------------------------------------------------------------- 
+
 // Compile top-level sass and autoprefix that jounce
 gulp.task('sass', function () {
-	return gulp.src('assets/src/sass/*.scss')
+	return gulp.src('assets/source/sass/*.scss')
 	.pipe(libsass({
 		outputStyle: 'nested', // only supports nested or compressed
 		sourceComments: 'normal'
@@ -28,7 +31,7 @@ gulp.task('sass', function () {
 
 // Compile top-level sass and compress it like whoa
 gulp.task('sass-build', function () {
-	return gulp.src('assets/src/sass/*.scss')
+	return gulp.src('assets/source/sass/*.scss')
 	.pipe(libsass({
 		outputStyle: 'compressed', // only supports nested or compressed
 	}))
@@ -39,34 +42,77 @@ gulp.task('sass-build', function () {
 	.pipe(gulp.dest('assets/build/css'));
 });
 
-// Concatenates all js
-gulp.task('concat', function(){
-	gulp.src([
+// Top level task to compile both third-party and custom JS
+gulp.task('concat', ['vendor-concat', 'js-concat']);
+
+// Concatenates third-party js
+gulp.task('vendor-concat', function(){
+	return gulp.src([
 		// Load bootstrap js in order
-		'./assets/src/js/plugins/bootstrap/transition.js',
-		'./assets/src/js/plugins/bootstrap/alert.js',
-		'./assets/src/js/plugins/bootstrap/button.js',
-		'./assets/src/js/plugins/bootstrap/carousel.js',
-		'./assets/src/js/plugins/bootstrap/collapse.js',
-		'./assets/src/js/plugins/bootstrap/dropdown.js',
-		'./assets/src/js/plugins/bootstrap/modal.js',
-		'./assets/src/js/plugins/bootstrap/tooltip.js',
-		'./assets/src/js/plugins/bootstrap/popover.js',
-		'./assets/src/js/plugins/bootstrap/scrollspy.js',
-		'./assets/src/js/plugins/bootstrap/tab.js',
-		'./assets/src/js/plugins/bootstrap/affix.js',
-		// Any other plugins?
-		'./assets/src/js/plugins/*.js',
-		// Load custom js
-		'./assets/src/js/main.js'
+		'./assets/source/vendor/bootstrap/js/transition.js',
+		'./assets/source/vendor/bootstrap/js/alert.js',
+		'./assets/source/vendor/bootstrap/js/button.js',
+		'./assets/source/vendor/bootstrap/js/carousel.js',
+		'./assets/source/vendor/bootstrap/js/collapse.js',
+		'./assets/source/vendor/bootstrap/js/dropdown.js',
+		'./assets/source/vendor/bootstrap/js/modal.js',
+		'./assets/source/vendor/bootstrap/js/tooltip.js',
+		'./assets/source/vendor/bootstrap/js/popover.js',
+		'./assets/source/vendor/bootstrap/js/scrollspy.js',
+		'./assets/source/vendor/bootstrap/js/tab.js',
+		'./assets/source/vendor/bootstrap/js/affix.js',
+
+		// Other plugins. Load more here as necessary
+		'./assets/source/vendor/flickity/flickity.pkgd.min.js',
+		'./assets/source/vendor/packery.pkgd.min.js',
+
+		// Turn on wildcard selector? Grabs any js file in source/vender
+		// './assets/source/vendor/*.js',
 	])
-	.pipe(concat('all.js'))
+	.pipe(concat('vendor.js'))
 	.pipe(gulp.dest('./assets/build/js/'));
 });
 
+// Concatenates all custom js
+gulp.task('js-concat', function(){
+    return gulp.src([
+        './assets/source/js/main.js',
+		// Any others? Load here as necessary
+        // './assets/source/js/*.js',
+    ])
+    .pipe(concat('custom.js'))
+    .pipe(gulp.dest('./assets/build/js'));    
+});
+
+
+// Static Assets
+// ---------------------------------------------------------------------------
+
+// Move static files only if there are changes
+gulp.task('static', ['vendor-fonts'], function(){
+	return gulp.src('assets/static/**/*')
+	.pipe(changed('assets/build'))
+	// extra pipes can go here if needed, ie image compression
+	.pipe(gulp.dest('assets/build'));
+});
+
+// Move fonts not in static folder
+gulp.task('vendor-fonts', function(){
+	return gulp.src([
+		'./assets/source/vendor/bootstrap/fonts/*',
+		// Place any other vendor fonts here:
+		// './assets/source/vendor/...',
+	])
+	.pipe(changed('assets/build/fonts'))
+	.pipe(gulp.dest('assets/build/fonts'));
+});
+
+// Prototyping Functions
+// ---------------------------------------------------------------------------
+
 // File includes
 gulp.task('include', function(){
-	gulp.src('./assets/src/templates/*.html')
+	gulp.src('./assets/source/templates/*.html')
 	.pipe(fileinclude())
 	.on('error', handleErrors)
 	.pipe(gulp.dest('./assets/build'))
@@ -83,37 +129,38 @@ gulp.task('server', function(){
 	console.log("Server listening at http://0.0.0.0:4000.");
 });
 
+
+// Watch and Build Functions
+// ---------------------------------------------------------------------------
+
 // Compiles sass and js, then minifies all js
-gulp.task('build', ['concat','sass-build','include', 'static'], function(){
+gulp.task('build', ['concat','sass-build','include','static'], function(){
 	gulp.src('assets/build/js/*.js')
 	.pipe(uglify())
 	.pipe(gulp.dest('assets/build/js'));
 });
 
-// Move static files only if there are changes
-gulp.task('static', function(){
-	return gulp.src('assets/static/**/*')
-	.pipe(changed('assets/build'))
-	// extra pipes can go here if needed, ie image compression
-	.pipe(gulp.dest('assets/build'));
-});
-
 // Watch for changes, recompile, and kick livereload
 gulp.task('watch', ['server'], function(){
-	gulp.watch('assets/src/sass/**/*.scss', ['sass']);
-	gulp.watch('assets/src/js/**/*.js', ['concat']);
+	gulp.watch('assets/source/sass/**/*.scss', ['sass']);
+	gulp.watch('assets/source/js/**/*.js', ['concat']);
 	gulp.watch('assets/static/**/*', ['static']);
-	gulp.watch('assets/src/templates/**/*', ['include']);
+	gulp.watch('assets/source/templates/**/*', ['include']);
 	
 	// tell livereload something has changed
 	livereload.listen();
 	gulp.watch('assets/build/**').on('change', livereload.changed);
 });
 
+// Default Task
 // Build everything and kick off the watch
+// ---------------------------------------------------------------------------
+
 gulp.task('default', ['concat', 'sass', 'include', 'static', 'watch']);
 
-// Error function
+// Error Handling
+// ---------------------------------------------------------------------------
+
 function handleErrors(){
   var args = Array.prototype.slice.call(arguments);
 
--- readme.md
+++ readme.md
@@ -2,47 +2,88 @@
 
 PI Boot + Basic HTML imports + LiveReload
 
+### Dependencies:
+
+- Node 4.2.1
+
+**(For Node 0.12 and lower, replace the `package.json` with [the older version](https://gist.github.com/nathanlong/1bcd85a4511c25d49f15) to ensure compatibility.)**
+
 ### To install:
 
-Install node
+Requires Node and a global install of Gulp.
 
-    brew install node
-
-Install gulp (only if you have node, but not gulp):
-
-    npm install --global gulp
-
-Install gulp and all its dependencies into the project (pulls from package.json):
+Navigate to project and run:
 
     npm install
 
-Then run gulp as normal:
+This will install the node packages. Then run gulp as normal:
 
     gulp [task name here]
 
-Boom. A static webserver will now show at:
-
-    http://localhost:4000
-    or
-    http://0.0.0.0:4000
-    or
-    http://127.0.0.1:4000
-
-(pick your poison, or find which one works if your `hosts` file is mangled).
+If you run `gulp` without any tasks specified, it will run the default task.
 
 ### Current Tasks:
 
 |Task Name|Description|
 |:--------|:----------|
-|default (no argument) | Builds all sass and js, then kicks off `watch` process |
+| default | Builds all sass and js, then kicks off `watch` process |
 | sass | compiles sass and autoprefixes for browser support |
 | sass-build | compiles sass, autoprefixes, and minifies |
-| include | stitches everything together from `assets/src/templates` with html includes |
-| server | fires up a local webserver at port 4000 |
-| concat | concatenates all js |
-| watch  | watches for changes in sass, js, or html templates, then recompiles/concatenates |
+| concat | fires off vendor-concat and  |
+| vendor-concat | specifies a list of javascript files to load, preserves order |
+| js-concat | specifies a list of custom javascript files to load, preserves order |
+| static | moves all static assets to `assets/build` and updates them if there are changes |
+| vendor-fonts | specifies a list of all vendor fonts to move to `assets/build/fonts`, this allows third-party files to stay together |
 | build | outputs compressed and minified versions of sass and js, used for push hook |
+| watch | will watch for any changes in custom files and regenerate on update |
+| include | stitches all html includes together |
+| server | fires up a local webserver at port 4000 |
 
+### Basic Structure
+
+All front-end assets are contained within the `/assets/` folder.
+
+There are three subfolders:
+
+- `/assets/build/` - contains all generated assets, sourcing should happen in this folder.
+- `/assets/source/` - contains all source files for generated assets, all work should happen in this folder
+- `/assets/static/` - contains all non-processed assets (images, fonts, etc)--things you don't want smushed together.
+
+The `source` folder, where all the work happens, is also split into three subfolders
+
+- `/assets/source/js` - all custom javascript.
+- `/assets/source/sass` - all custom SASS styles.
+- `/assets/source/vendor` - all third-party js, css, and sass.
+
+
+### Installing new plugins
+
+To install a new plugin into the project, download files and place them in `/assets/source/vendor/`.
+
+**For js files:**
+
+- Update the `vendor-concat` task in `gulpfile.js` to include the new plugin.
+
+**For css/scss files:**
+
+- Make sure they have the `.scss` file extension 
+- Include in `/assets/source/vendor/__vendor.scss`
+
+### Installing new fonts
+
+There are two ways to introduce new fonts into the framework:
+
+**Standalone:**
+
+Place the font files in `/assets/static/fonts/`. The `static` task will copy these to `/assets/build/fonts/` and can be sourced from the css as `../fonts/fontname.otf`, etc
+
+**Vendor:**
+
+If the fonts are included in another package (like Bootstrap) you can leave them inside their package and source them in the `vendor-fonts` task in the `gulpfile.js`
+
+This will grab all the fonts you specify and move them into the same location as the other fonts (`/assets/build/fonts`) for sourcing.
+
+----
 
 ### Differences from pi-boot
 
@@ -94,8 +135,7 @@ You can include as many variables as you'd like:
 @@include('partials/nav.html', {'nameOfVariable': 'nameOfAThing', 'anotherVariable': 'anotherThing'})
 ```
 
-
-
+----
 
 ### Extra Credit: LiveReload
 
